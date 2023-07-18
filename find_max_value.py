e94ce5e4ccd8b4990f52111cd1ca7ee62ca33255
mironshox2010 <mironshoxcodeschool@gmail.com>
2023-07-18 15:16:30 +0500

    find_max_value.py

--- find_max_value.py
+++ find_max_value.py
@@ -6,4 +6,13 @@ def find_max_value(data: dict):
     Returns:
         int: The maximum value in the dictionary.
     """
-    return 
+    lengh = 0
+    for item  in data.values:
+        lengh += len(item)
+    return lengh
+data = {
+    1: 'a', 
+    2: 'b', 
+    3: 'c'
+  }
+print(find_max_value(data))
