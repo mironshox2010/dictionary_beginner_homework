a78e05c852c1ff1ec47d6d46ec53576e02361b0b
mironshox2010 <mironshoxcodeschool@gmail.com>
2023-07-18 15:20:46 +0500

    get_max_age_user_name.py

--- get_max_age_user_name.py
+++ get_max_age_user_name.py
@@ -7,4 +7,12 @@ def get_max_age_user_name(data:list) -> str:
     Returns:
         str: The name of the user with the maximum age in the dictionary
     """
-    return 
+    mirka= ''
+    age=0
+    for i in data:
+        if i['age']>age:
+            age=i['age']
+            mirka=i['name']
+    return mirka
+print(get_max_age_user_name(data))
+    
